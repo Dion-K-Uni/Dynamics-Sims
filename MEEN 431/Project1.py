21fa8f36d60f46edd84fca2173fff5c0315dfeb0
Dion_K_Uni <a1849704@adelaide.edu.au>
2025-02-27 10:30:20 -0600

    changed to numpy

--- MEEN 431/Project1.py
+++ MEEN 431/Project1.py
@@ -1,38 +1,37 @@
-import sympy as sp
-from sympy import matrix 
+import numpy as np
 
 #define all functons to take in vectors for positions
 
 def ProcCalc(A):
     """Function to find U and V"""
-    
-    eVals = A.eigenvals()
-    eVec = A.eigenvects()
+
+    eigVals, eigVec = np.linalg.eig(A)
 
     normVec = []
-    for _, eig_vecs in eVec:
-        for vec in eig_vecs:
-            norm = vec.norm()
-            normVec.append(vec / norm)
+    for vec in eigVec.T:
+        norm = np.linalg.norm(vec)
+        normVec.append(vec/norm)
 
-    eigenvector_matrix = sp.Matrix.vstack(*normVec)
+    eigenvector_matrix = np.column_stack(normVec)
+
+    return eigenvector_matrix
 
 def RavgCalc(R1, R2):
     """R1, R2 are sympy matrices"""
 
     Ravg = 0.5*(R1+R2)
 
-    U = (Ravg*sp.transpose(Ravg))
-    V = (sp.transpose(Ravg)*Ravg)
+    U = (Ravg @ Ravg.T)
+    V = (Ravg.T @ Ravg)
 
-    R = V*sp.transpose(U)
+    R = V @ U.T
 
     return R
   
 def distanceCalc(GPoint, Bpoint):
     """Cartesian distance from points P, Q, R to h, g, f"""
 
-    res = sp.sqrt(((GPoint.x-Bpoint.x)^2)+((GPoint.y-Bpoint.y)^2)+((GPoint.z-Bpoint.z)^2))
+    res = np.sqrt(((GPoint.x-Bpoint.x)^2)+((GPoint.y-Bpoint.y)^2)+((GPoint.z-Bpoint.z)^2))
     return res
 
 
